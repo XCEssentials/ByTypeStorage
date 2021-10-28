3ad784af872cff3ccc3e964b66faa254a7482b88
Maxim Khatskevich <maxim@khatskevi.ch>
2021-10-28 03:27:43 +0200

    [ADD] ability to observe storage events within key type

--- Sources/Core/SomeKey.swift
+++ Sources/Core/SomeKey.swift
@@ -1,6 +1,24 @@
 /// Marker type for type that can be used as key in the storage.
 public
-protocol SomeKey {}
+protocol SomeKey
+{
+    @MainActor
+    static
+    var bindings: [StorageDispatcher.AccessEventBinding] { get }
+}
+
+// MARK: - Helpers
+
+public
+protocol NoBindings {}
+
+public
+extension NoBindings
+{
+    @MainActor
+    static
+    var bindings: [StorageDispatcher.AccessEventBinding] { [] }
+}
 
 // MARK: - Internal helpers
 
